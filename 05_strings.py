f39d86aa0d2bed49f92ac8d327ea137e54fee5ed
mahak-2001 <mahaktanwar171@gmail.com>
2026-09-20 18:03:44 +0530

    Update 05_strings.py

---
+++ 05_strings.py
@@ -0,0 +1,13 @@
+name="mahak rajput"
+
+print(name[1:4])
+print(name[-4:-1])
+print(name[1:8])
+print(name[1:8:2])
+
+print(len(name))
+print(name.endswith("put"))
+print(name.startswith("Mah"))
+print(name.capitalize())
+print(name.upper())
+print(name.find("rajput")) #this function finds a word and return the index of first occurance of that what in the string.
