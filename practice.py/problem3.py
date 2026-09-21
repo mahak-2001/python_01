fa45ed31a33ce72c09b8ab57c46b06c584c7290d
mahak-2001 <mahaktanwar171@gmail.com>
2026-09-21 23:26:08 +0530

    new changes

--- practice.py/problem3.py
+++ practice.py/problem3.py
@@ -1,15 +1,15 @@
-# fruits=[]
-# f1=input("enter fruit name:")
-# fruits.append(f1)
-# f2=input("enter fruit name:")           
-# fruits.append(f2)
-# f3=input("enter fruit name:")   
-# fruits.append(f3)
-# f4=input("enter fruit name:")
-# fruits.append(f4)   
-# f5=input("enter fruit name:")
-# fruits.append(f5)   
-# print(fruits)
+fruits=[]
+f1=input("enter fruit name:")
+fruits.append(f1)
+f2=input("enter fruit name:")           
+fruits.append(f2)
+f3=input("enter fruit name:")   
+fruits.append(f3)
+f4=input("enter fruit name:")
+fruits.append(f4)   
+f5=input("enter fruit name:")
+fruits.append(f5)   
+print(fruits)
 
 marks=[]
 m1=int(input("enter marks here:"))
@@ -25,6 +25,8 @@ marks.append(m5)
 marks.sort() 
 print(marks)
 
+a=[3,7,4,8]
+print(sum(a))
 
-# a=[3,7,4,8]
-# print(a)
+a=(7,0,8,0,0,9)
+print(a.count(0))
