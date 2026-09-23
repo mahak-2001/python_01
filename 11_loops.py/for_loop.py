8da9f918645dd1cd1d6305a5877dfe0a9cb8100b
mahak-2001 <mahaktanwar171@gmail.com>
2026-09-23 23:49:58 +0530

    add new programs

---
+++ 11_loops.py/for_loop.py
@@ -0,0 +1,37 @@
+for i in range(6):
+    print(i)
+
+#print list items:
+l=[1,2,3,5,6,879,87]
+for i in l:
+     print(i)
+     
+name="mahak"
+for i in name:
+    print(i)
+    
+# for loop with else:
+l=[1,2,5]
+for item in l:
+    print(item)
+else:
+    print("done")
+    
+for i in range(50):
+    if(i==25):
+        break #exit the loop now
+    print(i)
+
+for i in range(10):
+    if(i==5):
+        continue #exit the loop now
+    print(i)
+    
+for i in range(645):
+    pass
+    
+    
+i=0
+while(i<45):
+    print(i)
+    i+=1
---
+++ 11_loops.py/while_loop.py
@@ -0,0 +1,19 @@
+#while loop
+i=1
+while(i<5):
+    print(i)
+    i+=1
+    
+i=3
+while(i<31):
+    print(i)
+    i+=3
+    
+#list content:
+l=["mahak","rohan","mohan",78,66.89,False,3,4]
+# i=0
+# while(i<len(l)):
+#     print(l[i])
+#     i+=1
+    
+    
