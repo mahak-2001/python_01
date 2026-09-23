c6c2a506e3863f8e41060d700b671b215a337f9b
mahak-2001 <mahaktanwar171@gmail.com>
2026-09-23 22:57:44 +0530

    Update problem5.py

--- practice.py/problem5.py
+++ practice.py/problem5.py
@@ -32,4 +32,51 @@ if(total_percentage>=40 and mark1>33 and mark2>33 and mark3>33):
     print("you are passed:",total_percentage)
 else:
     print("you are failed,try again next year!")
-    
+
+#check spam comments in text,detect the spams.
+p1="makes a lot of money"
+p2="buy now"
+p3="subscribe this"
+p4="click this"
+message=input("enter your comment:")
+if(p1 in message or p2 in message or p3 in message or p4 in message):
+    print("this is a spam.")
+else:
+    print("this is not a spam.")
+
+#program to find given username contains less than 10 characters or not.
+username=input("enter name: ")
+if(len(username)<10):
+    print("username contain less than 10 characters.")
+else:
+    print("username contain more than 10 characters.")
+
+#find given name is present in list or not.
+l1=["mahak","jiya","teena","neha"]
+name=input("enter name:")
+if(name in l1):
+    print("name present in list.")
+else:
+    print("name is not present in list.")
+
+#calculate grade of a student from the marks.
+marks=int(input("enter marks: "))
+if(marks<=100 and marks>=90):
+    print("grade excellent.")
+elif(marks<90 and marks>=80):
+    print("grade A.")
+elif(marks<80 and marks>=70):
+    print("grade B.")
+elif(marks<70 and marks>=60):
+    print("grade C.")
+elif(marks<60 and marks>=50):
+    print("grade D.")
+else:
+    print("FAIL")
+
+#check given post is talking about name or not.
+post="hey mhk is good mahak is very good and mahak is great."
+if("mahak" in post):
+    print("post is talk about mahak.")
+else:
+    print("post is not talk about mahak.")
