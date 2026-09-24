c3495d61d3f6ba237013d2c86871ba018d92f42b
mahak-2001 <mahaktanwar171@gmail.com>
2026-09-24 22:27:48 +0530

    Update problem6.py

--- practice.py/problem6.py
+++ practice.py/problem6.py
@@ -41,5 +41,17 @@ for i in range(1, n+1):
     product = product*i
 print(f"the factorial of {n} is {product}")
 
-
+#program for making pattern
+'''
+for n=3
+  *
+ ***
+*****
+'''
+n=int(input("enter no.: "))
+for i in range(1,n+1):
+    print(" "*(n-1))
+    print("*"* (2*i-1),end="")
+    print("\n")
+    
     
