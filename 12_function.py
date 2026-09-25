c50a199b24ab9d0889cf2007f9a5e0b21c23df9e
mahak-2001 <mahaktanwar171@gmail.com>
2026-09-25 22:03:53 +0530

    Create 12_function.py

---
+++ 12_function.py
@@ -0,0 +1,16 @@
+# a=int(input("enter a no.: "))
+# b=int(input("enter a no.: "))
+# c=int(input("enter a no.: "))
+# average=((a+b+c)/3)
+# print(average)
+
+def avg():
+    a=int(input("enter a no.: "))
+    b=int(input("enter a no.: "))
+    c=int(input("enter a no.: "))   
+    average=((a+b+c)/3)
+    print(average)
+avg()
+avg()
+
+
