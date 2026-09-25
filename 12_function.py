431e4eec2eb8b677550974cf66e0b7b05abfc1f1
mahak-2001 <mahaktanwar171@gmail.com>
2026-09-25 22:47:57 +0530

    Update 12_function.py

--- 12_function.py
+++ 12_function.py
@@ -4,13 +4,31 @@
 # average=((a+b+c)/3)
 # print(average)
 
-def avg():
-    a=int(input("enter a no.: "))
-    b=int(input("enter a no.: "))
-    c=int(input("enter a no.: "))   
-    average=((a+b+c)/3)
-    print(average)
-avg()
-avg()
+# #function defination.
+# def avg():
+#     a=int(input("enter a no.: "))
+#     b=int(input("enter a no.: "))
+#     c=int(input("enter a no.: "))   
+#     average=((a+b+c)/3)
+#     print(average)
+# avg() #function call
+# print("thank you")
+# # avg()
 
+# def greet():
+#     name="mahak"
+#     print(f"Good Day!{name}")
+# greet()
 
+# def greet(name,ending):
+#     print(f"Good Day!{name}")
+#     print(ending)
+# greet("harry","thank you")
+# greet("mohan","thank you")
+# greet("divya","thanks")
+
+def greet(name):
+    gr ="hello"+name
+    return gr
+a=greet ("mahak")
+# a will contain hello mahak.
