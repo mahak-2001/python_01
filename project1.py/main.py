4b54c219847ebf7e6014e99efd9f58db04ee93ba
mahak-2001 <mahaktanwar171@gmail.com>
2026-09-27 01:08:25 +0530

    modify my project

--- project1.py/main.py
+++ project1.py/main.py
@@ -4,32 +4,40 @@ stone=1
 paper=-1
 scissor=0
 '''
-computer=1
-you=int(input("enter your choise: "))
-dict={
+def play_game(computer=1):
+ print("\n==========================ROCK PAPER SCISSORS==========================")
+ print("\n1.stone 🪨 (1)")
+ print("2.paper 📄 (-1)")
+ print("3.scissors ✂️ (0)\n")
+
+ you=int(input("Enter Your Choice: "))
+ dict={
     "stone":1,
     "paper":-1,
     "scissor":0
     }
-if(computer==you):
-    print("it's a draw.")
-elif(computer==1 and you==-1):
-    print("you choose=paper,computer choose=stone")
-    print("you win.")
-elif(computer==1 and you==0):
-    print("you choose=scissor,computer choose=stone")
-    print("you lose,computer win.")
-elif(computer==-1 and you==1):
-    print("you choose=stone,computer choose=paper")
-    print("you lose,computer win.")
-elif(computer==-1 and you==0):
-    print("you choose=scissor,computer choose=paper")
-    print("you win.")
-elif(computer==0 and you==1):
-    print("you choose:stone,computer choose=scissor")
-    print("you win.")
-elif(computer==0 and you==-1):
-    print("you choose=paper,computer choose=scissor")
-    print("you lose,computer win.")
-else:
-    print("something went wrong.")
+ if(computer==you):
+    print(" It's a Draw (=).")
+ elif(computer==1 and you==-1):
+    print("\n You chose= Paper📄.\n Computer chose= Stone🪨\n")
+    print("You win.🎉")
+ elif(computer==1 and you==0):
+    print("\n You chose= Scissor✂️\n Computer chose= Stone🪨\n")
+    print("You lose,computer win🎉.")
+ elif(computer==-1 and you==1):
+    print("\n You chose= Stone🪨.\n Computer chose= Paper📄\n")
+    print("You lose,Computer win🎉.")
+ elif(computer==-1 and you==0):
+    print("\n You chose= Scissor✂️.\n Computer chose= Paper📄\n")
+    print("You win.🎉")
+ elif(computer==0 and you==1):
+    print("\n You chose= Stone🪨.\n Computer chose= Scissor✂️\n")
+    print("You win.🎉")
+ elif(computer==0 and you==-1):
+    print("\n You chose= Paper📄.\n Computer chose= Scissor✂️\n")
+    print("You lose,Computer win🎉.")
+ else:
+    print("Something went wrong.")
+
+print("=======================================================================")
+play_game()
