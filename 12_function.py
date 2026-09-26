65fe5e1b63ceb4aaf029d6bbe729e5f096baeb0b
mahak-2001 <mahaktanwar171@gmail.com>
2026-09-26 18:24:37 +0530

    new programs

--- 12_function.py
+++ 12_function.py
@@ -1,31 +1,31 @@
-# a=int(input("enter a no.: "))
-# b=int(input("enter a no.: "))
-# c=int(input("enter a no.: "))
-# average=((a+b+c)/3)
-# print(average)
+a=int(input("enter a no.: "))
+b=int(input("enter a no.: "))
+c=int(input("enter a no.: "))
+average=((a+b+c)/3)
+print(average)
 
-# #function defination.
-# def avg():
-#     a=int(input("enter a no.: "))
-#     b=int(input("enter a no.: "))
-#     c=int(input("enter a no.: "))   
-#     average=((a+b+c)/3)
-#     print(average)
-# avg() #function call
-# print("thank you")
-# # avg()
+#function defination.
+def avg():
+    a=int(input("enter a no.: "))
+    b=int(input("enter a no.: "))
+    c=int(input("enter a no.: "))   
+    average=((a+b+c)/3)
+    print(average)
+avg() #function call
+print("thank you")
+# avg()
 
-# def greet():
-#     name="mahak"
-#     print(f"Good Day!{name}")
-# greet()
+def greet():
+    name="mahak"
+    print(f"Good Day!{name}")
+greet()
 
-# def greet(name,ending):
-#     print(f"Good Day!{name}")
-#     print(ending)
-# greet("harry","thank you")
-# greet("mohan","thank you")
-# greet("divya","thanks")
+def greet(name,ending):
+    print(f"Good Day!{name}")
+    print(ending)
+greet("harry","thank you")
+greet("mohan","thank you")
+greet("divya","thanks")
 
 def greet(name):
     gr="hello"+name
--- practice.py/problem7.py
+++ practice.py/problem7.py
@@ -19,7 +19,7 @@ print("nxt program :->>")
 #convert inch to cm.
 def inch(a):
     cm=2.54*a
-    print(f"{a} inch={cm} cm")
+    print(f"{a} inch = {cm} cm")
 inch(56)
 
 #sum of no.
@@ -27,4 +27,30 @@ def sum(n):
     if(n==1):
         return 1
     return sum(n-1)+n
-sum(6)
+print(sum(6))
+
+#print a pattern.
+def pattern(n):
+    if(n==0):
+        return 0
+    print("*"*n)
+    pattern(n-1)
+print(pattern(4))
+
+#table of a no.
+def table():
+    for i in range(1,11):
+        print(f"{n}*{i}={n*i}")
+     
+n=int(input("enter a no.: "))
+table()
+
+#function to remove a given word from a list and strip it at the same time.
+def list():
+    l1=["mahak","jiya","bhumi","sonu"]
+    l1.remove("jiya")
+    # l1.strip()
+    print(l1)
+list()
+    
+        
